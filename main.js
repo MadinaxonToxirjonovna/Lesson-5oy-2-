3eb7c7d9fda8cecb3a63299490116dca3c3b0e2a
Madinaxon Toxirjonovna <madinaxontoxirjonovna@gamil.com>
2024-04-11 22:01:36 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// const madina = {
+//     name:"Madina",
+//     yosh: 16,
+//     malumot: "oliy",
+//     school: 138,
+//     color: "black",
+//     hobby: qiziqish = {
+//         play:"tennis",
+//         homework: "homework",
+//         read:"read book"
+//     }
+// }
+
+// console.log(madina.hobby);
+
+
+// const computer = {
+//     browzer:"brovzer",
+//     mouse: "sichqoncha",
+//     keyboard: "klaviatura",
+//     google: "google",
+//     window: "window 10"
+// }
+
+// console.log(computer);
+
+
+//    UY ISHI
+let person = {
+    name: prompt("Ismingizni kiriting"),
+    id: prompt("space id kiriting"),
+    possword: prompt("space possword kiriting"),
+   }
+   console.log(person);
+
+
